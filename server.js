e95e0a2ddc167e7a943cb3390a72cd3c8b604794
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-07-17 22:26:25 +0530

    request and response

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 app.use(express.json());
 require('dotenv').config(); //it is neccessary to get the env variables from .env
 const dbConfig = require("./config/dbConfig");
 
 
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const usersRoute = require("./routes/usersRoute") 
 app.use("/api/users", usersRoute);
